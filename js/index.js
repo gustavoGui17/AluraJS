354b777a29846502a25ece6f415c42fb785ba53c
Gustavo Guilherme <88010932+gustavoGui17@users.noreply.github.com>
2023-03-06 18:06:25 -0300

    Calculando imc

--- js/index.js
+++ js/index.js
@@ -1,3 +1,26 @@
 var titulo = document.querySelector(".titulo");
-
 titulo.textContent = "Aparecida Nutri";
+
+var paciente = document.querySelector("#primeiro_paciente");
+
+var tdPeso = paciente.querySelector(".info-peso");
+var peso = tdPeso.textContent;
+
+var tdAltura = paciente.querySelector(".info-altura");
+var altura = tdAltura.textContent;
+
+var tdImc = paciente.querySelector(".info-imc");
+var imcDalinha = tdImc.textContent;
+
+var imc = peso / (altura * altura);
+
+
+if(peso < 0 || peso > 1000){
+    console.log("pesso invalido");
+}
+
+if(altura < 0 || altura > 3.00){
+    console.log("Altura errada")
+}
+
+tdImc.textContent = imc;
